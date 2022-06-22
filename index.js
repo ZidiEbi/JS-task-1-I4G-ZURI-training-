39fff3199372d2da71c20b0233bd90b4b599a4ff
Zidi Ebi <zididekeebi@gmail.com>
2022-06-22 21:50:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const firstName = 'Zidideke';
+const middleName = 'Justin';
+const lastName = 'Ebikake';
+const height = ' 5ft 10 inch' ;
+const country = 'Nigeria';
+
+name = + ' ' + firstName + ' ' + middleName + ' ' + lastName;
+
+document.write('Name:' + ' ' + firstName + ' ' + middleName + ' ' + lastName + '<br/>');
+document.write('Height:' + ' ' + height + '<br>');
+document.write('country:' + ' ' + country);
